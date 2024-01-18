4d27eb4a24c67f06fdb7b29cda1a5033bfd8be40
hectoragvz <hectoragvz@gmail.com>
2024-01-18 14:55:02 -0700

    Binary Search

---
+++ binarySearch.py
@@ -0,0 +1,18 @@
+def binary_sarch(array, target):
+    # defining indexes to look for
+    left = 0  # min index
+    right = len(array) - 1  # last index since start at 0
+
+    while left <= right:
+        # midpoint index
+        mid = (left + right) // 2
+        if array[mid] > target:
+            right = mid - 1
+        if array[mid] < target:
+            left = mid + 1
+        else:
+            return mid
+    return -1
+
+
+print(binary_sarch([-1, 0, 3, 5, 9, 12], 9))
