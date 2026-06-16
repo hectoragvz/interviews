d4bf5f85ad6573e51c97ae0998b144d9a01f6519
hectoragvz <hectoragvz@gmail.com>
2026-06-16 11:35:55 -0700

    trie impl

---
+++ tries/implementTrie.py
@@ -0,0 +1,33 @@
+class TrieNode:
+    def __init__(self):
+        self.isLast = False
+        self.children = {}
+
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        curr = self.root
+        for c in word:
+            if c not in curr.children:
+                curr.children[c] = TrieNode()
+            curr = curr.children[c]
+        curr.isLast = True  # Since it´s the end of a word
+
+    def search(self, word: str) -> bool:
+        curr = self.root
+        for c in word:
+            if c not in curr.children:
+                return False
+            curr = curr.children[c]
+        return curr.isLast
+
+    def startWith(self, word: str) -> bool:
+        curr = self.root
+        for c in word:
+            if c not in curr.children:
+                return False
+            curr = curr.children[c]
+        return True
